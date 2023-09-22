72a4230ff4fa427f15b3646a9eac2728115096b1
Luis Hernandez <sarco_luis@hotmail.com>
2023-09-21 19:25:15 -0600

    Declaracion de variables en VB.NET

--- miPrimerProyecto/miPrimerProyecto/Module1.vb
+++ miPrimerProyecto/miPrimerProyecto/Module1.vb
@@ -4,7 +4,17 @@
         Dim nombre As String
         nombre = "Luis Hernandez"
 
-        Console.Write("Hola " + nombre)
+        Dim edad As SByte
+        edad = 35
+
+        Dim cum As Double
+        cum = 9.5
+
+        Dim gustaProgramar As Boolean 'True o False
+        gustaProgramar = True
+
+        Console.Write("Mi nombre es: " + nombre + "; Mi edad: " + edad.ToString() + "; Me gusta programar: " +
+                      gustaProgramar.ToString() + "; Mi CUM: " + cum.ToString()) ' 25 => "25"
 
         Console.ReadLine()
     End Sub
