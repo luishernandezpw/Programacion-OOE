f650a8ab08be5213a9e83fcef199dfb69a75ad48
Luis Hernandez <sarco_luis@hotmail.com>
2023-09-21 19:48:11 -0600

    encontrar la nota final de computo

--- miPrimerProyecto/miPrimerProyecto/Module1.vb
+++ miPrimerProyecto/miPrimerProyecto/Module1.vb
@@ -1,18 +1,21 @@
 ﻿Module Module1
 
     Sub Main()
-        'realizar un programa en VB.NET que solicite dos numeros al usuario y muestre la suma de ellos.
-        Dim num1, num2, respuesta As Double
+        'realizar un programa en VB.NET que tome las notas Lab1(30%) + Lab2(30%) + P(40%) = NF Computo
+        Dim lab1, lab2, parcial, nf As Double
 
-        Console.Write("Num 1:")
-        num1 = Console.ReadLine()
+        Console.Write("Lab1: ")
+        lab1 = Console.ReadLine()
 
-        Console.Write("Num 2: ")
-        num2 = Console.ReadLine()
+        Console.Write("Lab2: ")
+        lab2 = Console.ReadLine()
 
-        respuesta = num1 + num2
+        Console.Write("Parcial: ")
+        parcial = Console.ReadLine()
 
-        Console.WriteLine("La suma es: " + respuesta.ToString())
+        nf = lab1 * 0.3 + lab2 * 0.3 + parcial * 0.4
+
+        Console.WriteLine("NF: " + nf.ToString())
 
         Console.ReadLine()
     End Sub
