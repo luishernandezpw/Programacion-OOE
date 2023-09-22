df2db88ed506edbfbd312c0956dec48017152536
Luis Hernandez <sarco_luis@hotmail.com>
2023-09-21 20:49:48 -0600

    realizar un programa en VB.NET que pida una table de multiplicar y la genere

--- miPrimerProyecto/miPrimerProyecto/Module1.vb
+++ miPrimerProyecto/miPrimerProyecto/Module1.vb
@@ -1,41 +1,18 @@
 ﻿Module Module1
 
     Sub Main()
-        'realizar un programa en VB.NET que pida la edad y realice la siguiente clasificacion:
-        'bebe 0 -2
-        'niño 3-11
-        'adolescente 12 - 17
-        'mayor de edad 18-65
-        'Adulto mayor de 66 - 80
-        'Larga vida 81 - x
+        'realizar un programa en VB.NET que pida una table de multiplicar y la genere:
+        '7x1 = 7
+        '7x2 = 14 ...
         While (True)
             Try
-                Dim edad As Int16
-                Console.Write("Edad: ")
-                edad = Console.ReadLine()
+                Dim ntabla, i As SByte
+                Console.Write("Tabla Multiplicar: ")
+                ntabla = Console.ReadLine()
 
-                Select Case edad
-                    Case 0 To 2
-                        Console.WriteLine("Eres un bebe")
-
-                    Case 3 To 11
-                        Console.WriteLine("Eres un niño")
-
-                    Case 12 To 17
-                        Console.WriteLine("Eres un adolescente")
-
-                    Case 18 To 65
-                        Console.WriteLine("Eres responsable de tus acciones")
-
-                    Case 66 To 80
-                        Console.WriteLine("Eres un adulto mayor, te ama tu familia")
-
-                    Case 81 To 127
-                        Console.WriteLine("Larga vidad")
-
-                    Case Else
-                        Console.WriteLine("Error en la edad")
-                End Select
+                For i = 1 To 10
+                    Console.WriteLine(ntabla.ToString() + "x" + i.ToString() + "=" + (ntabla * i).ToString())
+                Next
 
                 Console.Write("Deseas salir presiona (s), sino enter: ")
                 Dim salir = Console.ReadLine()
