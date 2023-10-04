d5cdf906202714c3f8da2973030654647b03fbe8
Luis Hernandez <sarco_luis@hotmail.com>
2023-10-03 20:31:17 -0600

    Resolucion del ejercicio de conversores

---
+++ pooe/pooe/pooe/Conversor.vb
@@ -0,0 +1,15 @@
+﻿Public Class Conversor
+    Public etiquetas As String()() = {
+        New String() {"Dolar", "Euro", "Quetzal", "Lempiras", "Colon SV", "Cordoba", "Colon CR"},
+        New String() {"Metro", "Cm", "Pulgada", "Pies", "Varas", "Yardas", "Km", "Millas"},
+        New String() {"Dia", "Segundos", "Minutos", "Horas", "Semana", "Mes", "Año", "Decada", "Siglo"}
+    }
+    Private valores As Double()() = {
+        New Double() {1, 0.96, 7.86, 24.67, 8.75, 36.6, 536.29},
+        New Double() {1, 100, 39.37, 3.28, 1.196308, 1.0936, 0.001, 0.000621371},
+        New Double() {1, 86400, 1440, 24, 0.142857, 0.0328767, 0.0027397280024383558, 0.00027397280024383559, 0.000027397280024383559}
+    }
+    Public Function convertir(ByVal tipo As SByte, ByVal de As SByte, ByVal a As SByte, ByVal cantidad As Double)
+        Return valores(tipo)(a) / valores(tipo)(de) * cantidad
+    End Function
+End Class
--- pooe/pooe/pooe/Form1.vb
+++ pooe/pooe/pooe/Form1.vb
@@ -1,3 +1,23 @@
 ﻿Public Class Form1
+    Dim objConversor As New Conversor()
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        cambiarConversor(0)
+    End Sub
+    Sub cambiarConversor(ByVal tipo As SByte)
+        cboDeConversor.Items.Clear()
+        cboAConversor.Items.Clear()
 
+        cboDeConversor.Items.AddRange(objConversor.etiquetas(tipo))
+        cboAConversor.Items.AddRange(objConversor.etiquetas(tipo))
+    End Sub
+
+    Private Sub cboTipoConversor_TextChanged(sender As Object, e As EventArgs) Handles cboTipoConversor.TextChanged
+        cambiarConversor(cboTipoConversor.SelectedIndex())
+    End Sub
+
+    Private Sub btnConvertir_Click(sender As Object, e As EventArgs) Handles btnConvertir.Click
+        lblRespuestaConversor.Text = Math.Round(objConversor.convertir(
+            cboTipoConversor.SelectedIndex, cboDeConversor.SelectedIndex, cboAConversor.SelectedIndex, txtCantidadConversor.Text
+        ), 2).ToString() + " " + objConversor.etiquetas(cboTipoConversor.SelectedIndex)(cboAConversor.SelectedIndex)
+    End Sub
 End Class
