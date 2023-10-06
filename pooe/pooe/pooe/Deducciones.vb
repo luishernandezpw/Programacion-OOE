caf0651f49ed8d2c35660bc9a93961e64ca65556
Luis Hernandez <sarco_luis@hotmail.com>
2023-10-05 20:49:42 -0600

    Desarrollo del calculo de las deducciones de ley isss, afp, isr, etc.

---
+++ pooe/pooe/pooe/Deducciones.vb
@@ -0,0 +1,50 @@
+﻿Public Class Deducciones
+    Public isssp, issse, afpp, afpe, isrp, vacacionesp, aguinaldop As Double
+    Sub calcular_deducciones(ByVal sueldo As Double, ByVal tiempo As Int16)
+        isssp = calcular(sueldo, 0.03)
+        issse = calcular(sueldo, 0.075)
+
+        afpp = calcular(sueldo, 0.0625)
+        afpe = calcular(sueldo, 0.0775)
+
+        isrp = isr(sueldo - isssp - afpp)
+        vacacionesp = vacaciones(sueldo)
+        aguinaldop = aguinaldo(sueldo, tiempo)
+    End Sub
+
+    Function calcular(ByVal sueldo As Double, ByVal porcentaje As Double)
+        Return sueldo * porcentaje
+    End Function
+
+    Function isr(ByVal sueldo As Double)
+        Dim isrval As Double, tabla(,) = {
+            {0.01, 472, 0, 0},
+            {472.01, 895.24, 0.1, 17.67},
+            {895.25, 2038.1, 0.2, 60},
+            {2038.11, 9999999, 0.3, 288.57}
+        }
+        For i = 0 To 3
+            If sueldo >= tabla(i, 0) And sueldo <= tabla(i, 1) Then
+                isrval = (sueldo - tabla(i, 0)) * tabla(i, 2) + tabla(i, 3)
+            End If
+        Next
+        Return isrval
+    End Function
+    Function vacaciones(ByVal sueldo As Double)
+        Return sueldo / 2 * 0.3
+    End Function
+    Function aguinaldo(ByVal sueldo As Double, ByVal tiempo As Int16)
+        Dim agui As Double
+        Select Case tiempo
+            Case 0
+                agui = tiempo * 1.1
+            Case 1
+                agui = sueldo / 30 * 10
+            Case 2
+                agui = sueldo / 30 * 15
+            Case 3
+                agui = sueldo / 30 * 18
+        End Select
+        Return agui
+    End Function
+End Class
--- pooe/pooe/pooe/Form1.vb
+++ pooe/pooe/pooe/Form1.vb
@@ -1,3 +1,40 @@
 ﻿Public Class Form1
+    Dim objDeduciones As New Deducciones
 
+    Private Sub btnCalcular_Click(sender As Object, e As EventArgs) Handles btnCalcular.Click
+        Try
+            Dim isssp, afpp, isrp, vacacionesp, aguinaldop, totalDeduciones, sueldoNeto As Double
+
+            objDeduciones.calcular_deducciones(txtSueldo.Text, cboTiempo.SelectedIndex)
+
+            isssp = objDeduciones.isssp
+            afpp = objDeduciones.afpp
+            isrp = objDeduciones.isrp
+            vacacionesp = objDeduciones.vacacionesp
+            aguinaldop = objDeduciones.aguinaldop
+            totalDeduciones = isssp + afpp + isrp
+            sueldoNeto = (txtSueldo.Text - totalDeduciones) + vacacionesp + aguinaldop
+
+            lblIsssEmpresas.Text = "ISSS: " + Math.Round(objDeduciones.issse, 2).ToString()
+            lblIsssPersonas.Text = "ISSS: " + Math.Round(isssp, 2).ToString()
+
+            lblAfpEmpresas.Text = "AFP: " + Math.Round(objDeduciones.afpe, 2).ToString()
+            lblAfpPersonas.Text = "AFP: " + Math.Round(afpp, 2).ToString()
+
+            lblIsrPersonas.Text = "ISR: " + Math.Round(isrp, 2).ToString()
+            lblVacacionesPersonas.Text = "VACACIONES: " + Math.Round(vacacionesp, 2).ToString()
+            lblAguinaldoPersonas.Text = "AGUINALDO: " + Math.Round(aguinaldop, 2).ToString()
+
+            lblTotalDeducciones.Text = "TOTAL DEDUCCIONES: " + Math.Round(totalDeduciones, 2).ToString()
+            lblSueldoNeto.Text = "SUELDO NETO: " + Math.Round(sueldoNeto, 2).ToString()
+
+            erpDeducciones.SetError(txtSueldo, "")
+        Catch ex As Exception
+            erpDeducciones.SetError(txtSueldo, "Por favor ingrese su sueldo.")
+        End Try
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        cboTiempo.SelectedIndex = 0
+    End Sub
 End Class
